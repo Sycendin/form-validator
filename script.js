3d2c88da307eb7082b8949c729e9b4cdb60a60d5
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-02 23:16:13 -0500

    adding finished files

--- script.js
+++ script.js
@@ -7,15 +7,56 @@ const message = document.querySelector("#message");
 
 
 let isValid = false;
+let passwordsMatch = false;
 const validateForm = () =>{
     isValid = form.checkValidity()
-    message.textContent = "Please fill out all fields"
-    message.style.color = 'red'
-    messageContainer.style.borderColor = 'red'
+    // Error message
+    if (!isValid){
+        message.textContent = "Please fill out all fields"
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        return;
+    }
+    if (password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green'
+        password2El.style.borderColor = 'green'
+    }
+    else{
+        passwordsMatch = false;
+        message.textContent = "Make sure passwords match"
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        password1El.style.borderColor = 'red'
+        password2El.style.borderColor = 'red'
+        return;
+
+    }
+    if (isValid && passwordsMatch){
+        message.textContent = "Sucessfully registered."
+        message.style.color = 'green'
+        messageContainer.style.borderColor = 'green'
+    }
+}
+
+const storeFormData = () =>{
+    const user = {
+        name:form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+    console.log(user)
 }
 const processFormData=(e)=>{
     e.preventDefault();
+    // validate form
     validateForm();
+    //Submit if valid
+    if (isValid && passwordsMatch){
+        storeFormData();
+    }
 }
 //Event listeners
 form.addEventListener('submit', processFormData)
